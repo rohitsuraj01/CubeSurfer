daab81c536db237c794774a821c932c4c99bde91
Rohit Vudata <rohitvudata@users.noreply.github.com>
2020-10-30 09:22:27 +0530

    Created New Project

---
+++ Cube Surfer/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    bool gameHasEnded = false;
+    public float restartDelay = 1f;
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+    public GameObject completeLevelUI;
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Cube Surfer/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovement movement;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+        if (collision.collider.tag == "cube")
+        {
+            Debug.Log("Collision Detected");
+            Destroy(collision.collider);
+        }
+    }
+}
